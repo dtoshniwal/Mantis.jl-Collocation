00d35298192a3984aeffb653eb29d8df95ca5b13
Diogo C. Cabanas <dccabanas-dev@tuta.com>
2026-06-01 10:07:41 +0200

    fix: update `get_derivative_idx` call

--- src/Forms/FormOperators/PartialDerivative.jl
+++ src/Forms/FormOperators/PartialDerivative.jl
@@ -133,7 +133,7 @@ function evaluate(
     form_eval, form_indices = _evaluate_form_in_canonical_coordinates(
         form, element_id, xi, num_derivatives
     )
-    der_idx = FunctionSpaces.get_derivative_idx([partial_orders...])
+    der_idx = FunctionSpaces.get_derivative_idx(partial_orders)
     partial_der_eval = form_eval[num_derivatives + 1][der_idx]
     partial_der_eval = _add_geometric_scaling!(
         partial_der_eval, partial_der, element_id, xi, partial_orders
@@ -153,7 +153,7 @@ function evaluate(
     form_eval, form_indices = _evaluate_form_in_canonical_coordinates(
         get_form_space(form), element_id, xi, num_derivatives
     )
-    der_idx = FunctionSpaces.get_derivative_idx([partial_orders...])
+    der_idx = FunctionSpaces.get_derivative_idx(partial_orders)
     partial_der_eval = [
         form_eval[num_derivatives + 1][der_idx][1] *
         view(get_coefficients(form), form_indices[1]),
